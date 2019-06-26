1722880f13256454fba83125ddd13c5e50eb1193
fais24 <52233659+fais24@users.noreply.github.com>
2019-06-26 10:30:03 +0300

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main (String args []) {
 		System.out.println("Hello World");
 		System.out.println("im adding code to my project");
+		System.out.println("adding code from github");
 	}
 }
