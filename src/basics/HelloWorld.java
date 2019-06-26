4a53e59bce8312646b964b58842cc6e6e815843c
fais24 <fpatch@protonmail.ch>
2019-06-26 10:33:31 +0300

    updated

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("im adding code to my project");
 		System.out.println("adding code from github");
+		System.out.println("test again");
 	}
 }
